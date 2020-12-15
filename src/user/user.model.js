c6af43f75eb36ac03d984050320c6ad38130cec6
lafch <albeflores@gmail.com>
2020-12-14 19:11:54 -0500

    aaaa

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: String,
   lastName: String,
-  dateOfBirth: Number,
+  dateOfBirth: String,
   phone: Number,
   email:String,
   address: String,
